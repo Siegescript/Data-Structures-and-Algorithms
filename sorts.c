f97a030975f6769bc2576b35fe6cd5186983317a
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2024-09-19 22:05:07 +0800

    listing sorts and function prototypes of sorts.

---
+++ sorts.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void selectionSort();
+
+void insertionSort();
+void shellSort();
+
+void quickSort();
+int partition();
+
+void mergeSort();
+void merge();
+
+void countSort();
+
+void radixSort();
+void bucketSort();
+
+void gnomeSort();
+
+void strandSort();
+
+void heapify();
+void heapSort();
+void tournamentSort();
+
+void bubbleSort();
+void combSort();
+
+int main()
+{
+
+
+    return 0;
+}
