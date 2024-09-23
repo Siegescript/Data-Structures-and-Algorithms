e0d9afda8ad88556747d2a590357a732aefacf38
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2024-09-23 11:34:37 +0800

    created function to generate a large and small array

--- sorts.c
+++ sorts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define SMALL_SIZE 10
+#define LARGE_SIZE 1000
+#define MAX_ELEMENT_VALUE 999
+
+void generateArray(int*, int, int);
+void printArray(int*, int);
+
 void selectionSort();
 
 void insertionSort();
@@ -30,7 +37,16 @@ void combSort();
 
 int main()
 {
-
+    int smallArray[SMALL_SIZE];
+    int largeArray[LARGE_SIZE];
 
     return 0;
+}
+
+void generateArray(int *arr, int size, int max)
+{
+    for(int n = 0; n < size; n++)
+    {
+        arr[n] = rand() % max;
+    }
 }
