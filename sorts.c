752041833bade14c8ccb4a2b61aa153274a27c5e
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2024-09-29 19:18:06 +0800

    completed heap sort function

--- sorts.c
+++ sorts.c
@@ -25,9 +25,8 @@ void gnomeSort();
 
 void strandSort();
 
-void heapify();
-int deleteMin();
-void heapSort();
+void heapify(int[], int, int);
+void heapSort(int[], int);
 void tournamentSort();
 
 void bubbleSort(int[], int);
@@ -106,6 +105,43 @@ void shellSort(int arr[], int size)
             }
             arr[ndx] = key;
         }
+    }
+}
+
+void heapify(int Heap[], int size, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if(left < size && Heap[left] > Heap[right])
+    {
+        largest = left;
+    }
+
+    if(right < size && Heap[right] > Heap[largest])
+    {
+        largest = right;
+    }    
+
+    if(largest != i) 
+    {
+        swap(&Heap[i], &Heap[largest]);
+        heapify(Heap, size, largest);
+    }
+}
+
+void heapSort(int arr[], int size)
+{
+    for (int i = size / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, size, i);
+    }
+  
+    for (int i = size - 1; i >= 0; i--)
+    {
+      swap(&arr[0], &arr[i]);
+      heapify(arr, i, 0);
     }
 }
 
