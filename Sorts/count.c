8d3f557e1ff8a156544645794cf28ddec0174ef1
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2024-10-06 20:54:32 +0800

    commented cummulative sum implementation

--- Sorts/count.c
+++ Sorts/count.c
@@ -38,6 +38,8 @@ int main()
 void countSort(int arr[], int size)
 {
     int max = arr[0];
+    // int output[size];
+
     for(int i = 0; i < size; i++)
     {
         if(max < arr[i])
@@ -45,7 +47,7 @@ void countSort(int arr[], int size)
             max = arr[i];
         }
     }
-    
+
     int count[max + 1];
 
     for(int i = 0; i <= max; ++i) 
@@ -57,6 +59,22 @@ void countSort(int arr[], int size)
     {
         count[arr[i]]++;
     }
+
+    // for(int i = 1; i <= max; i++) 
+    // {
+    //     count[i] += count[i - 1];
+    // }
+
+    // for(int i = size - 1; i >= 0; i--) 
+    // {
+    //     output[count[arr[i]] - 1] = arr[i];
+    //     count[arr[i]]--;
+    // }
+
+    // for(int i = 0; i < size; i++) 
+    // {
+    //     arr[i] = output[i];
+    // }
 
     for (int i = 0, j = 0; i <= max; i++)
     {
