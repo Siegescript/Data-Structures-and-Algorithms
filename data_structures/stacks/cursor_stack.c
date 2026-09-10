489d2af0cba90cebf053f9c3b22e1359cb2d420e
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-09-11 01:59:07 +0800

    refactor: update cursor stack data structure

--- data_structures/stacks/cursor_stack.c
+++ data_structures/stacks/cursor_stack.c
@@ -1,98 +1,132 @@
-#include<stdio.h>
-#define MAX 5
-#define null -1
+#include <stdio.h>
+#include <stdbool.h>
 
-typedef int STACK;
+#define MAX 10
+#define NULL_LINK -1
 
-typedef struct node
-{
-    char data;
-    STACK link;
-}NODE;
+typedef char ElementType;
+typedef int StackPos;
 
-typedef struct heap
-{
-    NODE elem[MAX];
-    STACK avail;
-}HEAP;
+typedef struct node {
+    ElementType data;
+    StackPos link;
+} Node;
 
-STACK alloc(HEAP* H);
-void dealloc(HEAP* H, STACK del);
-void initHeap(HEAP* H);
+typedef struct heap {
+    Node elem[MAX];
+    StackPos avail;
+} Heap;
 
-void initStack(STACK* S);
-void push(HEAP* H,STACK* S,char elem);
-void pop(HEAP* H,STACK* S);
-char top(HEAP H,STACK S);
+// Core API
+void initHeap(Heap *H);
+void initStack(StackPos *S);
+void freeStack(Heap *H, StackPos *S);
+void push(Heap *H, StackPos *S, ElementType elem);
+void pop(Heap *H, StackPos *S);
+ElementType top(Heap H, StackPos S);
 
-int main()
-{
-    HEAP H;
-    STACK S;
+// Utility
+bool isEmpty(StackPos S);
+void display(Heap H, StackPos S);
+
+// Internal Memory Helpers
+static StackPos allocNode(Heap *H) {
+    StackPos ret = H->avail;
+    if (ret != NULL_LINK) {
+        H->avail = H->elem[ret].link;
+        H->elem[ret].link = NULL_LINK;
+    }
+    return ret;
+}
+
+static void deallocNode(Heap *H, StackPos del) {
+    if (del < 0 || del >= MAX) return;
+    H->elem[del].link = H->avail;
+    H->avail = del;
+}
+
+int main(void) {
+    Heap H;
+    StackPos S;
+
     initHeap(&H);
     initStack(&S);
     
-    push(&H,&S,'K');
-    push(&H,&S,'C');
-    push(&H,&S,'U');
-    push(&H,&S,'F');
+    push(&H, &S, 'K');
+    push(&H, &S, 'C');
+    push(&H, &S, 'U');
+    push(&H, &S, 'F');
 
-    printf("Top element in stack: %c\n",top(H,S));
-    pop(&H,&S);
-    printf("Top element in stack: %c\n",top(H,S));
+    printf("--- Cursor Stack Contents ---\n");
+    display(H, S);
+
+    printf("\nTop element: %c\n", top(H, S));
+    pop(&H, &S);
+    printf("Top element after pop: %c\n", top(H, S));
+
+    freeStack(&H, &S);
 
     return 0;
 }
 
-STACK alloc(HEAP* H)
-{
-    STACK ret=H->avail;
-    H->avail=H->elem[ret].link;
-    H->elem[ret].link=null;
-    return ret;
+void initHeap(Heap *H) {
+    for (int i = MAX - 1; i >= 0; i--) {
+        H->elem[i].link = i - 1;
+    }
+    H->avail = MAX - 1;
 }
 
-void dealloc(HEAP* H,STACK del)
-{
-    H->elem[del].link=H->avail;
-    H->avail=del;
+void initStack(StackPos *S) {
+    *S = NULL_LINK;
 }
 
-void initHeap(HEAP* H)
-{
-    int ndx=MAX-1;
-    H->avail=ndx;
-    for(;ndx>=0;ndx--)
-    {
-        H->elem[ndx].link=ndx-1;
+void freeStack(Heap *H, StackPos *S) {
+    while (!isEmpty(*S)) {
+        pop(H, S);
     }
 }
 
-void initStack(STACK* S)
-{
-    *S=null;
-}
-
-void push(HEAP* H,STACK* S, char elem)
-{
-    STACK ndx=alloc(H);
-    H->elem[ndx].data=elem;
-    H->elem[ndx].link=*S;
-    *S=ndx;
-}
-
-void pop(HEAP* H,STACK* S)
-{
-    STACK del;
-    if(*S!=null)
-    {
-        del=*S;
-        *S=H->elem[*S].link;
-        dealloc(H,del);
+void push(Heap *H,StackPos *S, ElementType elem) {
+    StackPos newNode = allocNode(H);
+    if (newNode == NULL_LINK) {
+        printf("ERROR: Heap overflow. Virtual heap is full.\n");
+        return;
     }
+    H->elem[newNode].data=  elem;
+    H->elem[newNode].link = *S;
+    *S = newNode;
 }
 
-char top(HEAP H,STACK S)
-{
+void pop(Heap *H, StackPos *S) {
+    if (isEmpty(*S)) {
+        printf("ERROR: Stack underflow. Cannot pop from an empty stack.\n");
+        return;
+    }
+    StackPos del = *S;
+    *S = H->elem[*S].link;
+    deallocNode(H, del);
+}
+
+ElementType top(Heap H, StackPos S) {
+    if (isEmpty(S)) {
+        printf("ERROR: Stack is empty. Returning null character.\n");
+        return '\0';
+    }
     return H.elem[S].data;
+}
+
+bool isEmpty(StackPos S) {
+    return S == NULL_LINK;
+}
+
+void display(Heap H, StackPos S) {
+    if (isEmpty(S)) {
+        printf("STACK IS EMPTY\n");
+        return;
+    }
+    printf("TOP -> ");
+    for (StackPos trav = S; trav != NULL_LINK; trav = H.elem[trav].link) {
+        printf("[%c | idx:%d] ", H.elem[trav].data, trav);
+    }
+    printf("<- BOTTOM\n");
 }
